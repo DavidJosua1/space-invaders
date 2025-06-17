62c5e1817741826a70e41c7d920637f7f19ce5b2
limon salvaje <davidjosuemamaniparillo@gmail.com>
2025-06-16 19:03:20 -0500

    movimiento del avion en la recta horizontal

---
+++ avioncito.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <iostream>
+#include <windows.h> 
+#include <conio.h>
+using namespace std;
+
+void dibujar_avion(int x){
+    
+    string espacio(x, ' ');
+
+    string Avion_11 = "  A";
+    string Avion_12 = " /-\\";
+    string Avion_13 = "<o o>";
+    string Avion_14 = " o o";
+
+    cout<<espacio<<Avion_11<<endl;
+    cout<<espacio<<Avion_12<<endl;
+    cout<<espacio<<Avion_13<<endl;
+    cout<<espacio<<Avion_14<<endl;
+}
+
+int main(){
+    int x=0;
+
+     while (true) {
+        system ("cls"); 
+        dibujar_avion(x);
+        
+        char tecla = getch();
+        if (tecla =='a'&&x>0){
+            x--;
+        }else if(tecla=='d'&&x<151){
+            x++;
+        }else if(tecla==27){
+            break;
+        }
+    }
+
+}
