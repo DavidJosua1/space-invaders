12a6a709647aa6a5ce70ad2153c0899bac296f61
limon salvaje <davidjosuemamaniparillo@gmail.com>
2025-06-19 17:06:12 -0500

    enemigo y sistema de colision con las balas

--- avioncito.cpp
+++ avioncito.cpp
@@ -1,10 +1,10 @@
 #include <string>
 #include <iostream>
 #include <windows.h> 
-#include <conio.h>
+#include <time.h>
 using namespace std;
 
-//achho=156 lineas
+//ancho=156 lineas
 //alto=41 filas
 
 void dibujar_avion(int x){
@@ -25,11 +25,23 @@ void dibujar_disparo(int x){
     string espacio(x, ' ');
     cout<<espacio<<"|"<<endl;
 }
-
+void dibujar_enemigo(int x){
+    string espacio(x, ' ');
+    cout<<espacio<<"(o_o)"<<endl;
+    cout<<espacio<<"<   >"<<endl;
+    cout<<espacio<<"/---\\"<<endl;
+}
 int main(){
     int x=0,y=36;
+    //-1 indica el estado en el que no se encuentra algo en la pantalla, ya sea el enemigo o el disparo
     int x1=-1,y1=-1;
+    int x2=-1,y2=-1;
+    //definimos semilla aleatoria para la aparicion de enemigos aleatorios
+    srand(time(NULL));
+    //contador para el enemigo
+    int cont=0;
      while (true) {
+        //imprimir la pantalla
         system ("cls"); 
         for(int i=0;i<40;i++){
             if(i==y){
@@ -37,11 +49,14 @@ int main(){
                 i+=3;
             }else if(i==y1&&y1!=-1){
                 dibujar_disparo(x1);
+            }else if(i==y2&&y2!=-1){
+                dibujar_enemigo(x2);
+                i+=2;   
             }else{
                 cout<<endl;
             }
         }
-
+        //tema_controles de movimiento
         if (GetAsyncKeyState('A') & 0x8000 && x > 0) x-=2;
         if (GetAsyncKeyState('D') & 0x8000 && x < 152) x+=2;
         if (x < 0) x = 0;
@@ -52,7 +67,7 @@ int main(){
         if(y==37){
             y=36;
         }
-        if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) break; // ESC para salir
+        //controles para el disparo
         if (GetAsyncKeyState(' ') & 0x8000 && x1==-1&&y1==-1){
             x1=x+2;
             y1=y-1;
@@ -64,7 +79,49 @@ int main(){
             x1=-1;
             y1=-1;         
         }
+        //enemigo
+        //el enemigo aparecera en una posicion aleatoria
+        if(x2==-1&&y2==-1){
+            x2=rand()%152;
+            y2=rand()%15;
+        }
 
-    Sleep(30);
+        cont++;
+        //el enemigo descendera desde su posicion
+        if(x2!=-1&&y2!=-1&&cont==4){
+            y2++;
+        }
+        //reiniciamos el contador
+        if(cont==4){
+            cont=0;
+        }
+        //colision por fuera de campo
+        if(y2>40){
+            x2=-1;
+            y2=-1;
+        }
+        //colision por bala
+    
+        //Colision en x
+        bool colision_en_x=false;
+        if(x1>=x2&&x1<=x2+4){
+            colision_en_x=true;
+        }
+        //Colision en y
+        bool colision_en_y=false;
+        if(y2<=y1&&y1<=y2+2){
+            colision_en_y=true;
+        }
+
+        //Colision total
+        if(colision_en_x&&colision_en_y){
+            x2=-1;
+            y2=-1;
+            x1=-1;
+            y1=-1;
+        }
+
+        if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) break; // ESC para salir
+        Sleep(30);
     }
 }
