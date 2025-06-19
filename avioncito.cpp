1cbb15cba37b60184f7b318e70ac5458b6521557
limon salvaje <davidjosuemamaniparillo@gmail.com>
2025-06-18 20:34:09 -0500

    movimiento dinamico del avion

--- avioncito.cpp
+++ avioncito.cpp
@@ -4,7 +4,7 @@
 #include <conio.h>
 using namespace std;
 
-void dibujar_avion(int x){
+void dibujar_avion(int x, int y){
     
     string espacio(x, ' ');
 
@@ -13,6 +13,10 @@ void dibujar_avion(int x){
     string Avion_13 = "<o o>";
     string Avion_14 = " o o";
 
+    for(int i=0;i<y;i++){
+        cout<<"\n";
+    }
+
     cout<<espacio<<Avion_11<<endl;
     cout<<espacio<<Avion_12<<endl;
     cout<<espacio<<Avion_13<<endl;
@@ -20,19 +24,14 @@ void dibujar_avion(int x){
 }
 
 int main(){
-    int x=0;
-
+    int x=0,y=35;
      while (true) {
         system ("cls"); 
-        dibujar_avion(x);
-        
-        char tecla = getch();
-        if (tecla =='a'&&x>0){
-            x--;
-        }else if(tecla=='d'&&x<151){
-            x++;
-        }else if(tecla==27){
-            break;
-        }
+        dibujar_avion(x,y);
+        if (GetAsyncKeyState('A') & 0x8000 && x > 0) x--;
+        if (GetAsyncKeyState('D') & 0x8000 && x < 151) x++;
+        if (GetAsyncKeyState('W') & 0x8000 && y > 1) y--;
+        if (GetAsyncKeyState('S') & 0x8000 && y < 36) y++;
+        if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) break; // ESC para salir
     }
 }
