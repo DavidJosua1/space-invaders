adc21dfb80d3bdef49dfdaf2290a1b4cb5c7cd92
limon salvaje <davidjosuemamaniparillo@gmail.com>
2025-06-18 23:06:29 -0500

    logramos hacer que el avion dispare

--- avioncito.cpp
+++ avioncito.cpp
@@ -4,7 +4,10 @@
 #include <conio.h>
 using namespace std;
 
-void dibujar_avion(int x, int y){
+//achho=156 lineas
+//alto=41 filas
+
+void dibujar_avion(int x){
     
     string espacio(x, ' ');
 
@@ -13,25 +16,55 @@ void dibujar_avion(int x, int y){
     string Avion_13 = "<o o>";
     string Avion_14 = " o o";
 
-    for(int i=0;i<y;i++){
-        cout<<"\n";
-    }
-
     cout<<espacio<<Avion_11<<endl;
     cout<<espacio<<Avion_12<<endl;
     cout<<espacio<<Avion_13<<endl;
     cout<<espacio<<Avion_14<<endl;
 }
+void dibujar_disparo(int x){
+    string espacio(x, ' ');
+    cout<<espacio<<"|"<<endl;
+}
 
 int main(){
-    int x=0,y=35;
+    int x=0,y=36;
+    int x1=-1,y1=-1;
      while (true) {
         system ("cls"); 
-        dibujar_avion(x,y);
-        if (GetAsyncKeyState('A') & 0x8000 && x > 0) x--;
-        if (GetAsyncKeyState('D') & 0x8000 && x < 151) x++;
-        if (GetAsyncKeyState('W') & 0x8000 && y > 1) y--;
-        if (GetAsyncKeyState('S') & 0x8000 && y < 36) y++;
+        for(int i=0;i<40;i++){
+            if(i==y){
+                dibujar_avion(x);
+                i+=3;
+            }else if(i==y1&&y1!=-1){
+                dibujar_disparo(x1);
+            }else{
+                cout<<endl;
+            }
+        }
+
+        if (GetAsyncKeyState('A') & 0x8000 && x > 0) x-=2;
+        if (GetAsyncKeyState('D') & 0x8000 && x < 152) x+=2;
+        if (x < 0) x = 0;
+        if (x > 151) x = 151;
+
+        if (GetAsyncKeyState('W') & 0x8000 && y > 2) y--;
+        if (GetAsyncKeyState('S') & 0x8000 && y < 37) y++;
+        if(y==37){
+            y=36;
+        }
         if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) break; // ESC para salir
+        if (GetAsyncKeyState(' ') & 0x8000 && x1==-1&&y1==-1){
+            x1=x+2;
+            y1=y-1;
+        } 
+        if(y1!=-1){
+            y1--;
+        }
+        if(y1!=-1&&y1<1){  
+            x1=-1;
+            y1=-1;         
+        }
+
+    Sleep(30);
     }
 }
